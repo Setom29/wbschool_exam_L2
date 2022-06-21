b7a0b55416173c5f901b433060bf9212adc7f4aa
Setom29 <sergeymtolstov35@gmail.com>
2022-06-21 14:22:31 +0300

    dev02 done

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"strconv"
 	"unicode"
 )
 
@@ -26,6 +25,65 @@ import (
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
+var ErrInvalidString = errors.New("invalid string")
+
+func Unpack(s string) (string, error) {
+	sr := []rune(s)
+	var s2 string
+	var n int
+	var backslash bool
+
+	for i, item := range sr {
+		// the [0] element is digit
+		if unicode.IsDigit(item) && i == 0 {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(item) && unicode.IsDigit(sr[i-1]) && sr[i-2] != '\\' {
+			return "", ErrInvalidString
+		}
+		// adding backslash
+		if item == '\\' && !backslash {
+			backslash = true
+			continue
+		}
+		// letter after backslash
+		if backslash && unicode.IsLetter(item) {
+			return "", ErrInvalidString
+		}
+		if backslash {
+			s2 += string(item)
+			backslash = false
+			continue
+		}
+		if unicode.IsDigit(item) {
+			n = int(item - '0')
+			if n == 0 {
+				s2 = s2[:len(s2)-1]
+				continue
+			}
+			for j := 0; j < n-1; j++ {
+				s2 += string(sr[i-1])
+			}
+			continue
+		}
+		s2 += string(item)
+	}
+
+	return s2, nil
+}
+
+func main() {
+	s := `qwe\4\5`
+	ans, err := Unpack(s)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	} else {
+		fmt.Println(ans)
+	}
+}
+
+/*
 func appendLetters(r []rune, l rune, digit []rune) []rune {
 	num, err := strconv.Atoi(string(digit))
 	if err != nil {
@@ -78,14 +136,4 @@ func getFullString(s string) (string, error) {
 		}
 	}
 	return string(arr), nil
-}
-
-func main() {
-	s := "a4bc2d5e"
-	ans, err := getFullString(s)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	} else {
-		fmt.Println(ans)
-	}
-}
+}*/
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -6,8 +6,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// empty string
 func TestEmptyString(t *testing.T) {
-	s, err := getFullString("")
+	s, err := Unpack("")
 	if err != nil {
 		t.Error(err)
 	}
@@ -17,7 +18,7 @@ func TestEmptyString(t *testing.T) {
 }
 
 func TestCustomString(t *testing.T) {
-	s, err := getFullString("a4bc2d5e")
+	s, err := Unpack("a4bc2d5e")
 	okStr := "aaaabccddddde"
 	if err != nil {
 		t.Error(err)
@@ -28,8 +29,41 @@ func TestCustomString(t *testing.T) {
 }
 
 func TestInvalidString(t *testing.T) {
-	s, err := getFullString("45")
+	s, err := Unpack("45")
 	if err == nil {
 		t.Errorf(`Expected "Invalid string", got "%s"`, s)
+	}
+}
+
+func TestBackspaceString1(t *testing.T) {
+	s, err := Unpack(`qwe\4\5`)
+	okStr := "qwe45"
+	if err != nil {
+		t.Error(err)
+	}
+	if !assert.Equal(t, okStr, s) {
+		t.Errorf(`Expected "%s", got "%s"`, okStr, s)
+	}
+}
+
+func TestBackspaceString2(t *testing.T) {
+	s, err := Unpack(`qwe\45`)
+	okStr := `qwe44444`
+	if err != nil {
+		t.Error(err)
+	}
+	if !assert.Equal(t, okStr, s) {
+		t.Errorf(`Expected "%s", got "%s"`, okStr, s)
+	}
+}
+
+func TestBackspaceString3(t *testing.T) {
+	s, err := Unpack(`qwe\\5`)
+	okStr := `qwe\\\\\`
+	if err != nil {
+		t.Error(err)
+	}
+	if !assert.Equal(t, okStr, s) {
+		t.Errorf(`Expected "%s", got "%s"`, okStr, s)
 	}
 }
