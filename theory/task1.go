f1f8da9c8d98570b92623b968670880269188dcb
Setom29 <sergeymtolstov35@gmail.com>
2022-07-13 15:55:54 +0300

    some changes

--- theory/task1.go
+++ theory/task1.go
@@ -1,13 +1,63 @@
 package main
 
-func main() {
-	ch := make(chan int)
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func asChan(vs ...int) <-chan int {
+	c := make(chan int)
 	go func() {
-		for i := 0; i < 10; i++ {
-			ch <- i
+		for _, v := range vs {
+			c <- v
+			time.Sleep(time.Duration(rand.Intn(1000)) *
+				time.Millisecond)
 		}
+		close(c)
 	}()
-	for n := range ch {
-		println(n)
+	return c
+}
+func merge(a, b <-chan int) <-chan int {
+	c := make(chan int)
+	go func() {
+		for {
+			select {
+			case v := <-a:
+				c <- v
+			case v := <-b:
+				c <- v
+			}
+		}
+
+	}()
+	return c
+}
+
+func main() {
+	a := asChan(1, 3, 5, 7)
+	b := asChan(2, 4, 6, 8)
+	c := merge(a, b)
+	for v := range c {
+		fmt.Println(v)
 	}
 }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	s := make([]int, 0)
+// 	var oldCap int
+// 	for i := 0; i < 100000; i++ {
+// 		s = append(s, i)
+// 		if cap(s) != oldCap {
+// 			if oldCap != 0 {
+// 				fmt.Println(i, oldCap, float64(cap(s))/float64(oldCap))
+// 			}
+// 			oldCap = cap(s)
+// 		}
+// 	}
+
+// }
